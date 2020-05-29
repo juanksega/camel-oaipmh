af20b6f8d1e937daef4cd4b3c79779e622cf7363
Juan Segarra <juanksegarraf@gmail.com>
2020-05-29 00:34:53 -0500

    Fix an issue with HTTPS in an oai_dc.xsd dependency

--- src/main/java/es/upm/oeg/camel/oaipmh/component/OAIPMHEndpoint.java
+++ src/main/java/es/upm/oeg/camel/oaipmh/component/OAIPMHEndpoint.java
@@ -11,47 +11,94 @@ import org.joda.time.format.ISODateTimeFormat;
  * Represents a OAIPMH endpoint.
  */
 @Data
-@EqualsAndHashCode(callSuper=true)
+@EqualsAndHashCode(callSuper = true)
 public class OAIPMHEndpoint extends DefaultPollingEndpoint {
 
-    private final String url;
+  private final String url;
 
-    @UriParam
-    private String from;
-    @UriParam
-    private String until;
-    @UriParam
-    private String set;
-    @UriParam
-    private String verb = "ListRecords";
-    @UriParam
-    private String metadataPrefix = "oai_dc";
+  @UriParam
+  private String from;
+  @UriParam
+  private String until;
+  @UriParam
+  private String set;
+  @UriParam
+  private String verb = "ListRecords";
+  @UriParam
+  private String metadataPrefix = "oai_dc";
 
+  public OAIPMHEndpoint(String uri, String url, OAIPMHComponent component) {
+    super(uri, component);
+    this.url = url;
+  }
 
-    public OAIPMHEndpoint(String uri, String url, OAIPMHComponent component) {
-        super(uri, component);
-        this.url = url;
+  public Producer createProducer() throws Exception {
+    return new OAIPMHProducer(this);
+  }
+
+  public Consumer createConsumer(Processor processor) throws Exception {
+    OAIPMHConsumer consumer = new OAIPMHConsumer(this, processor);
+    validateParameters();
+    configureConsumer(consumer);
+    return consumer;
+  }
+
+  private void validateParameters() {
+    // From parameter in ISO 8601 format
+    if (from != null) {
+      ISODateTimeFormat.dateTimeNoMillis().parseDateTime(from);
     }
-
-    public Producer createProducer() throws Exception {
-        return new OAIPMHProducer(this);
+    if (until != null) {
+      ISODateTimeFormat.dateTimeNoMillis().parseDateTime(until);
     }
+  }
 
-    public Consumer createConsumer(Processor processor) throws Exception {
-        OAIPMHConsumer consumer = new OAIPMHConsumer(this, processor);
-        validateParameters();
-        configureConsumer(consumer);
-        return consumer;
-    }
+  public boolean isSingleton() {
+    return true;
+  }
 
+  public String getUrl() {
+    return url;
+  }
 
-    private void validateParameters(){
-        // From parameter in ISO 8601 format
-        if (from != null) ISODateTimeFormat.dateTimeNoMillis().parseDateTime(from);
-        if (until != null) ISODateTimeFormat.dateTimeNoMillis().parseDateTime(until);
-    }
+  public String getFrom() {
+    return from;
+  }
 
-    public boolean isSingleton() {
-        return true;
-    }
+  public void setFrom(String from) {
+    this.from = from;
+  }
+
+  public String getUntil() {
+    return until;
+  }
+
+  public void setUntil(String until) {
+    this.until = until;
+  }
+
+  public String getSet() {
+    return set;
+  }
+
+  public void setSet(String set) {
+    this.set = set;
+  }
+
+  public String getVerb() {
+    return verb;
+  }
+
+  public void setVerb(String verb) {
+    this.verb = verb;
+  }
+
+  public String getMetadataPrefix() {
+    return metadataPrefix;
+  }
+
+  public void setMetadataPrefix(String metadataPrefix) {
+    this.metadataPrefix = metadataPrefix;
+  }
+
 }
